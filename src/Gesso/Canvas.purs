432e96cbc0b9b9896ac8eff94199fd96e786b513
Tom Smilack <tom@smilack.net>
2020-09-18 02:09:31 -0500

    Use single-event emmiters to fire animation frames instead of a recursive loop.
    I don't think the loop would've been able to access updated app state, also it was ticking for no reason. Add an event listener for window resizing, but it doesn't do anything yet. Also clean up some stuff - State doesn't need to be newtype; Input's record doesn't need a type alias; get rid of pickContext because if we don't get the context during Initialize we probably never will; get rid of some unnecessary fields in State.

--- src/Gesso/Canvas.purs
+++ src/Gesso/Canvas.purs
@@ -12,86 +12,86 @@ import CSS as CSS
 import Color as Color
 import Data.Maybe (Maybe(..))
 import Halogen.Query.EventSource as ES
+import Halogen.Query.EventSource (eventListenerEventSource, effectEventSource)
 import Data.Traversable (traverse_, sequence)
 import Data.Foldable (sequence_)
 import Gesso.Window as Window
 import Web.HTML (window)
+import Web.HTML.Window (document)
 import Effect.Aff.Class (class MonadAff)
 import Graphics.Canvas as Canvas
 import Data.Newtype (class Newtype, unwrap)
 import Gesso.Dimensions as Dims
+import Web.Event.Event (EventType(..), Event)
+import Web.HTML.HTMLDocument as HTMLDocument
 
 data RenderStyle appState
   = NoRender
-  | Continuous (Number -> appState -> Context2D -> Effect Unit)
+  | Continuous (Number -> Number -> appState -> Context2D -> Effect Unit)
   | OnChange (appState -> Context2D -> Effect Unit)
 
-data Action appState
-  = Start
-  | Stop
-  | Tick
+data Action
+  = Initialize
+  | Finalize
+  | HandleResize Event
+  | Tick (Maybe Number)
+
+data Query appState a
+  = UpdateAppState appState a
 
 newtype Input appState
-  = Input (InitialInput appState)
-
-type InitialInput appState
-  = { boundingBox :: Dims.Dimensions
-    , renderFn :: RenderStyle appState
-    , appState :: appState
-    }
-
-newtype State appState
-  = State
-  { viewBox :: Dims.Dimensions
-  , clientRect :: Maybe Dims.ClientRect
+  = Input
+  { boundingBox :: Dims.Dimensions
   , renderFn :: RenderStyle appState
-  , tickSub :: Maybe H.SubscriptionId
-  , time :: Number
-  , frames :: Number
-  , context :: Maybe Canvas.Context2D
-  , name :: String
   , appState :: appState
   }
 
-derive instance newtypeState :: Newtype (State appState) _
+type State appState
+  = { viewBox :: Dims.Dimensions
+    , clientRect :: Maybe Dims.ClientRect
+    , renderFn :: RenderStyle appState
+    , frames :: Number
+    , context :: Maybe Canvas.Context2D
+    , resizeSub :: Maybe H.SubscriptionId
+    , name :: String
+    , appState :: appState
+    }
 
 component ::
-  forall query appState output m.
+  forall appState output m.
   MonadAff m =>
-  H.Component HH.HTML query (Input appState) output m
+  H.Component HH.HTML (Query appState) (Input appState) output m
 component = H.mkComponent { initialState, render, eval }
   where
   eval ::
     forall slots.
     MonadAff m =>
-    H.HalogenQ query (Action appState) (Input appState)
-      ~> H.HalogenM (State appState) (Action appState) slots output m
+    H.HalogenQ (Query appState) Action (Input appState)
+      ~> H.HalogenM (State appState) Action slots output m
   eval =
     H.mkEval
       ( H.defaultEval
           { handleAction = handleAction
-          , initialize = Just Start
+          , initialize = Just Initialize
           }
       )
 
 initialState :: forall appState. Input appState -> State appState
 initialState (Input { boundingBox, renderFn, appState }) =
-  State
-    { viewBox: boundingBox
-    , clientRect: Nothing
-    , renderFn
-    , tickSub: Nothing
-    , time: 0.0
-    , frames: 0.0
-    , context: Nothing
-    , name: "screen"
-    , appState
-    }
+  { viewBox: boundingBox
+  , clientRect: Nothing
+  , renderFn
+  , frames: 0.0
+  , resizeSub: Nothing
+  , context: Nothing
+  , name: "screen"
+  , appState
+  }
 
 render ::
   forall appState slots m.
-  State appState -> H.ComponentHTML (Action appState) slots m
-render (State { viewBox, name }) =
+  State appState -> H.ComponentHTML Action slots m
+render { viewBox, name } =
   HH.canvas
     $ [ HP.id_ name
       , HS.style do
@@ -103,72 +103,56 @@ render (State { viewBox, name }) =
 handleAction ::
   forall appState slots output m.
   MonadAff m =>
-  Action appState ->
-  H.HalogenM (State appState) (Action appState) slots output m Unit
+  Action -> H.HalogenM (State appState) Action slots output m Unit
 handleAction = case _ of
-  Start -> do
-    handleAction Stop
-    renderFn <- H.gets $ _.renderFn <<< unwrap
-    name <- H.gets $ _.name <<< unwrap
-    stateContext <- H.gets $ _.context <<< unwrap
-    mcontext <- H.liftEffect $ pickContext stateContext name
-    appState <- H.gets $ _.appState <<< unwrap
-    subscription <-
+  Finalize -> pure unit
+  Initialize -> do
+    name <- H.gets _.name
+    mcontext <- H.liftEffect $ getContext name
+    document <- H.liftEffect $ document =<< window
+    resizeSub <-
       H.subscribe
-        $ ES.effectEventSource \emitter -> do
-            ref <- Ref.new Nothing
-            let
-              rAFLoop :: Number -> Effect Unit
-              rAFLoop time = do
-                case renderFn of
-                  NoRender -> pure unit
-                  Continuous fn -> do
-                    ES.emit emitter Tick
-                    sequence_ $ fn time appState <$> mcontext
-                  OnChange fn -> do
-                    sequence_ $ fn appState <$> mcontext
-                id <- Window.requestAnimationFrame rAFLoop =<< window
-                Ref.write (Just id) ref
-            id1 <- Window.requestAnimationFrame rAFLoop =<< window
-            Ref.write (Just id1) ref
-            ES.emit emitter Tick
-            pure
-              $ ES.Finalizer do
-                  Ref.read ref
-                    >>= traverse_ \id ->
-                        Window.cancelAnimationFrame id =<< window
-    let
-      updateSub :: H.SubscriptionId -> State appState -> State appState
-      updateSub subId (State state) = (State state { tickSub = Just subId })
-    H.modify_ (updateSub subscription)
-  Stop -> do
-    subscription <- H.gets $ _.tickSub <<< unwrap
-    traverse_ unsubscribe subscription
-  Tick -> pure unit --H.modify_ $ (\st -> State $ st { frames = st.frames + 1.0 }) <<< unwrap
+        $ eventListenerEventSource
+            (EventType "resize")
+            (HTMLDocument.toEventTarget document)
+            (Just <<< HandleResize)
+    H.modify_ \state -> state { context = mcontext, resizeSub = Just resizeSub }
+    handleAction $ Tick Nothing
+  HandleResize event -> pure unit
+  Tick mLastTime -> do
+    animationFrame mLastTime
 
-unsubscribe ::
+animationFrame ::
   forall appState slots output m.
   MonadAff m =>
-  H.SubscriptionId ->
-  H.HalogenM (State appState) (Action appState) slots output m Unit
-unsubscribe subscription = do
-  H.unsubscribe subscription
-  H.modify_ updateSub
-  where
-  updateSub :: State appState -> State appState
-  updateSub (State state) = (State state { tickSub = Nothing })
+  Maybe Number -> H.HalogenM (State appState) Action slots output m Unit
+animationFrame mLastTime = do
+  mcontext <- H.gets _.context
+  appState <- H.gets _.appState
+  renderFn <- H.gets _.renderFn
+  _ <-
+    H.subscribe' \_ ->
+      effectEventSource \emitter -> do
+        _ <-
+          window
+            >>= Window.requestAnimationFrame \timestamp -> do
+                case renderFn of
+                  NoRender -> pure unit
+                  OnChange fn -> sequence_ $ fn appState <$> mcontext
+                  Continuous fn -> do
+                    case mLastTime of
+                      Nothing -> pure unit
+                      Just lastTime -> do
+                        let
+                          delta = timestamp - lastTime
+                        sequence_ $ fn timestamp delta appState <$> mcontext
+                    ES.emit emitter $ Tick $ Just timestamp
+                ES.close emitter
+        mempty
+  pure unit
 
 getContext :: String -> Effect (Maybe Canvas.Context2D)
 getContext name = do
   mcanvas <- Canvas.getCanvasElementById name
   mcontext <- sequence $ Canvas.getContext2D <$> mcanvas
   pure mcontext
-
-pickContext ::
-  Maybe Canvas.Context2D -> String -> Effect (Maybe Canvas.Context2D)
-pickContext stateContext name = case stateContext of
-  Nothing -> do
-    mcontext <- getContext name
-    pure mcontext
-  Just context -> do
-    pure $ Just context
--- src/Main.purs
+++ src/Main.purs
@@ -8,7 +8,6 @@ import Gesso.Canvas as Gesso.Canvas
 import Graphics.Canvas as Canvas
 import Math (cos, sin)
 import Gesso.Dimensions as Dims
-import Gesso.Graphics as Graphics
 
 main :: Effect Unit
 main =
@@ -21,9 +20,11 @@ type AppState
 
 init :: Gesso.Canvas.Input AppState
 init =
-  Gesso.Canvas.initialize
-    $ { origin: Graphics.origin
-      , dimensions: Dims.fromWidthAndHeight 300.0 300.0
+  Gesso.Canvas.Input
+    $ { boundingBox:
+          Dims.fromPointAndSize
+            Dims.origin
+            (Dims.fromWidthAndHeight { width: 300.0, height: 300.0 })
       , renderFn
       , appState: { color: "blue" }
       }
@@ -31,11 +32,11 @@ init =
 renderFn :: Gesso.Canvas.RenderStyle AppState
 renderFn = Gesso.Canvas.Continuous render
   where
-  render :: Number -> AppState -> Canvas.Context2D -> Effect Unit
-  render time { color } context = do
+  render :: Number -> Number -> AppState -> Canvas.Context2D -> Effect Unit
+  render timestamp delta { color } context = do
     Canvas.clearRect context { x: 0.0, y: 0.0, width: 300.0, height: 300.0 }
     Canvas.setFillStyle context color
     Canvas.fillRect context { x: 100.0, y: 50.0, width: 200.0, height: 25.0 }
     let
-      t = time * 6.28 / 1000.0 / 2.0
+      t = timestamp * 6.28 / 1000.0 / 2.0
     Canvas.fillRect context { x: 150.0 + 50.0 * cos t, y: 150.0 + 50.0 * sin t, width: 10.0, height: 10.0 }
