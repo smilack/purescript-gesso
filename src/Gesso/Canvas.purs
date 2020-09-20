619998cdcf2e922494fb48f43af42412cdcca89c
Tom Smilack <tom@smilack.net>
2020-09-19 23:35:48 -0500

    Take Origin & Aspect Ratio as input, pass actual viewbox as parameter to render fn

--- src/Gesso/Canvas.purs
+++ src/Gesso/Canvas.purs
@@ -4,11 +4,12 @@ import Prelude
 import CSS as CSS
 import Data.Foldable (sequence_, traverse_)
 import Data.Function (on)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Data.Traversable (traverse)
 import Debug.Trace (trace, traceM, spy)
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
+import Gesso.AspectRatio as AR
 import Gesso.Dimensions as Dims
 import Gesso.Time as T
 import Graphics.Canvas (Context2D, getCanvasElementById, getContext2D)
@@ -28,7 +29,7 @@ import Web.UIEvent.MouseEvent (MouseEvent)
 
 data RenderStyle appState
   = NoRender
-  | Continuous (appState -> T.Delta -> Context2D -> Effect Unit)
+  | Continuous (appState -> T.Delta -> Dims.ViewBox -> Context2D -> Effect Unit)
   | OnChange (appState -> Context2D -> Effect Unit)
 
 data Action
@@ -48,14 +49,17 @@ data Query appState a
 
 newtype Input appState
   = Input
-  { boundingBox :: Dims.Dimensions
+  { origin :: Dims.Point
+  , aspectRatio :: AR.AspectRatio
   , renderFn :: RenderStyle appState
   , appState :: appState
   }
 
 type State appState
-  = { viewBox :: Dims.Dimensions
+  = { origin :: Dims.Point
+    , aspectRatio :: AR.AspectRatio
     , clientRect :: Maybe Dims.ClientRect
+    , viewBox :: Maybe Dims.ViewBox
     , renderFn :: RenderStyle appState
     , canvas :: Maybe HTMLElement
     , context :: Maybe Context2D
@@ -80,9 +84,11 @@ component =
     }
 
 initialState :: forall appState. Input appState -> State appState
-initialState (Input { boundingBox, renderFn, appState }) =
-  { viewBox: boundingBox
+initialState (Input { origin, aspectRatio, renderFn, appState }) =
+  { origin
+  , aspectRatio
   , clientRect: Nothing
+  , viewBox: Nothing
   , renderFn
   , resizeSub: Nothing
   , canvas: Nothing
@@ -92,31 +98,39 @@ initialState (Input { boundingBox, renderFn, appState }) =
   }
 
 render :: forall appState slots m. State appState -> H.ComponentHTML Action slots m
-render { viewBox, name } =
+render { clientRect, name } =
   canvas
     $ [ id_ name
       , style fullscreenStyle
       , HE.onMouseMove getCursorCoordinates
       ]
-    <> Dims.toSizeProps viewBox
+    <> maybe [] Dims.toSizeProps clientRect
 
 getCursorCoordinates :: MouseEvent -> Maybe Action
 getCursorCoordinates = Just <<< MouseMoveEvent <<< Dims.fromMouseEvent
 
-handleQuery :: forall appState a slots output m. MonadAff m => Query appState a -> H.HalogenM (State appState) Action slots output m (Maybe a)
+handleQuery ::
+  forall appState a slots output m.
+  MonadAff m =>
+  Query appState a ->
+  H.HalogenM (State appState) Action slots output m (Maybe a)
 handleQuery (UpdateAppState appState a) = do
   H.modify_ (_ { appState = appState })
   pure $ Just a
 
-handleAction :: forall appState slots m. MonadAff m => Action -> H.HalogenM (State appState) Action slots Output m Unit
+handleAction ::
+  forall appState slots m.
+  MonadAff m =>
+  Action ->
+  H.HalogenM (State appState) Action slots Output m Unit
 handleAction = case _ of
   Initialize -> do
     initialize
     handleAction $ Tick Nothing
   HandleResize -> updateClientRect
   Tick mLastTime -> do
-    { context, appState, renderFn } <- H.get
-    queueAnimationFrame mLastTime context appState renderFn
+    { context, appState, renderFn, viewBox } <- H.get
+    queueAnimationFrame mLastTime context viewBox appState renderFn
   Finalize -> unsubscribeResize
   AnimationFrameStart delta -> H.raise $ FrameStart delta
   MouseMoveEvent p -> H.raise $ MouseMove p
@@ -124,17 +138,28 @@ handleAction = case _ of
 initialize :: forall appState slots output m. MonadAff m => H.HalogenM (State appState) Action slots output m Unit
 initialize = do
   resizeSub <- subscribeResize
-  name <- H.gets _.name
+  { name, origin, aspectRatio } <- H.get
   mcontext <- H.liftEffect $ getContext name
   mcanvas <- H.liftEffect $ getCanvasElement name
   clientRect <- H.liftEffect $ getCanvasClientRect mcanvas
-  H.modify_ (_ { context = mcontext, resizeSub = Just resizeSub, clientRect = clientRect, canvas = mcanvas })
+  H.modify_
+    ( _
+        { context = mcontext
+        , resizeSub = Just resizeSub
+        , clientRect = clientRect
+        , canvas = mcanvas
+        , viewBox = Dims.getViewBox origin aspectRatio <$> clientRect
+        }
+    )
 
 queueAnimationFrame ::
   forall appState slots output m.
   MonadAff m =>
-  Maybe (T.Timestamp T.Prev) -> Maybe Context2D -> appState -> RenderStyle appState -> H.HalogenM (State appState) Action slots output m Unit
-queueAnimationFrame mLastTime context appState renderFn = do
+  Maybe (T.Timestamp T.Prev) ->
+  Maybe Context2D ->
+  Maybe Dims.ViewBox ->
+  appState -> RenderStyle appState -> H.HalogenM (State appState) Action slots output m Unit
+queueAnimationFrame mLastTime context viewBox appState renderFn = do
   _ <- H.subscribe $ ES.effectEventSource rafEventSource
   pure unit
   where
@@ -158,7 +183,7 @@ queueAnimationFrame mLastTime context appState renderFn = do
         --   will be or if there are any alternatives.
         -- Possibly by having the parent pass in the state update function?
         traverse_ (ES.emit emitter <<< AnimationFrameStart) mdelta
-        sequence_ $ fn appState <$> mdelta <*> context
+        sequence_ $ fn appState <$> mdelta <*> viewBox <*> context
         ES.emit emitter $ Tick $ Just $ T.toPrev timestamp
     ES.close emitter
 
@@ -179,11 +204,19 @@ getCanvasClientRect mcanvas = do
   (mbounding :: Maybe DOMRect) <- traverse getBoundingClientRect mcanvas
   pure $ Dims.fromDOMRect <$> mbounding
 
-updateClientRect :: forall appState action slots output m. MonadAff m => H.HalogenM (State appState) action slots output m Unit
+updateClientRect ::
+  forall appState action slots output m.
+  MonadAff m =>
+  H.HalogenM (State appState) action slots output m Unit
 updateClientRect = do
-  mcanvas <- H.gets _.canvas
-  clientRect <- H.liftEffect $ getCanvasClientRect mcanvas
-  H.modify_ (_ { clientRect = clientRect })
+  { canvas, origin, aspectRatio } <- H.get
+  clientRect <- H.liftEffect $ getCanvasClientRect canvas
+  H.modify_
+    ( _
+        { clientRect = clientRect
+        , viewBox = Dims.getViewBox origin aspectRatio <$> clientRect
+        }
+    )
 
 fullscreenStyle :: CSS.CSS
 fullscreenStyle = do
@@ -194,12 +227,18 @@ fullscreenStyle = do
   CSS.top $ CSS.pct 50.0
   CSS.transform $ CSS.translate (CSS.pct $ -50.0) (CSS.pct $ -50.0)
 
-unsubscribeResize :: forall appState action slots output m. MonadAff m => H.HalogenM (State appState) action slots output m Unit
+unsubscribeResize ::
+  forall appState action slots output m.
+  MonadAff m =>
+  H.HalogenM (State appState) action slots output m Unit
 unsubscribeResize = do
   mresizeSub <- H.gets _.resizeSub
   traverse_ H.unsubscribe mresizeSub
 
-subscribeResize :: forall appState slots output m. MonadAff m => H.HalogenM (State appState) Action slots output m H.SubscriptionId
+subscribeResize ::
+  forall appState slots output m.
+  MonadAff m =>
+  H.HalogenM (State appState) Action slots output m H.SubscriptionId
 subscribeResize = do
   wnd <- H.liftEffect window
   H.subscribe
--- src/Main.purs
+++ src/Main.purs
@@ -7,7 +7,8 @@ import Data.Maybe (Maybe(..))
 import Data.Newtype (unwrap)
 import Debug.Trace (trace, traceM, spy)
 import Effect (Effect)
-import Gesso.Canvas as Gesso.Canvas
+import Gesso.AspectRatio as AR
+import Gesso.Canvas as GC
 import Gesso.Dimensions as Dims
 import Gesso.Time as T
 import Graphics.Canvas as Canvas
@@ -20,18 +21,18 @@ main :: Effect Unit
 main =
   runHalogenAff do
     body <- awaitBody
-    io <- runUI Gesso.Canvas.component init body
+    io <- runUI GC.component init body
     io.subscribe
       $ CR.consumer
       $ case _ of
-          Gesso.Canvas.FrameStart delta -> do
+          GC.FrameStart delta -> do
             pure Nothing
-          Gesso.Canvas.MouseMove p -> do
+          GC.MouseMove p -> do
             let
               x = Dims.getX p
 
               y = Dims.getY p
-            _ <- io.query $ H.tell $ Gesso.Canvas.UpdateAppState (initialState { mouse = Just { x, y } })
+            _ <- io.query $ H.tell $ GC.UpdateAppState (initialState { mouse = Just { x, y } })
             pure Nothing
     -- -- io.dispose - kill Halogen app
     pure unit
@@ -44,29 +45,36 @@ type AppState
 initialState :: AppState
 initialState = { color: "blue", mouse: Nothing }
 
-init :: Gesso.Canvas.Input AppState
+init :: GC.Input AppState
 init =
-  Gesso.Canvas.Input
-    $ { boundingBox:
-          Dims.fromPointAndSize
-            Dims.origin
-            (Dims.fromWidthAndHeight { width: 2560.0, height: 1440.0 })
+  GC.Input
+    $ { origin: Dims.origin
+      , aspectRatio: AR.w1h1
       , renderFn
       , appState: initialState
       }
 
-renderFn :: Gesso.Canvas.RenderStyle AppState
-renderFn = Gesso.Canvas.Continuous render
+renderFn :: GC.RenderStyle AppState
+renderFn = GC.Continuous render
   where
-  render :: AppState -> T.Delta -> Canvas.Context2D -> Effect Unit
-  render { color, mouse } { now } context = do
+  render :: AppState -> T.Delta -> Dims.ViewBox -> Canvas.Context2D -> Effect Unit
+  render { color, mouse } { now } viewBox context = do
     Canvas.setFillStyle context "#DDFFDD"
-    Canvas.fillRect context { x: 0.0, y: 0.0, width: 2560.0, height: 1440.0 }
+    Canvas.fillRect context { x: scaleW 5.0, y: scaleH 5.0, width: scaleW 630.0, height: scaleH 630.0 }
     Canvas.setFillStyle context color
-    Canvas.fillRect context { x: 100.0, y: 50.0, width: 200.0, height: 25.0 }
-    let
-      t = (unwrap now) * 6.28 / 1000.0 / 2.0
-    Canvas.fillRect context { x: 150.0 + 50.0 * cos t, y: 150.0 + 50.0 * sin t, width: 10.0, height: 10.0 }
+    Canvas.fillRect context { x: scaleW 100.0, y: scaleH 50.0, width: scaleW 200.0, height: scaleH 25.0 }
+    Canvas.fillRect context
+      { x: scaleW $ 150.0 + 50.0 * cos t
+      , y: scaleH $ 150.0 + 50.0 * sin t
+      , width: scaleW 10.0
+      , height: scaleH 10.0
+      }
     traverse_ (Canvas.fillRect context) (mouseRect <$> mouse)
     where
-    mouseRect { x, y } = { x, y, width: 30.0, height: 30.0 }
+    mouseRect { x, y } = { x, y, width: scaleW 30.0, height: scaleH 30.0 }
+
+    t = (unwrap now) * 6.28 / 1000.0 / 2.0
+
+    scaleW = (_ / 640.0) <<< (_ * Dims.getWidth viewBox)
+
+    scaleH = (_ / 640.0) <<< (_ * Dims.getHeight viewBox)
