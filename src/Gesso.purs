574f1af51b250ad72f83c79fa66cffb01626825b
Tom Smilack <tom@smilack.net>
2020-09-28 00:59:47 -0500

    Clean up run function and add a function to run under a different monad

--- src/Gesso.purs
+++ src/Gesso.purs
@@ -2,6 +2,7 @@ module Gesso
   ( module Halogen.Aff
   , runGessoAff
   , run
+  , runWith
   , Input
   ) where
 
@@ -14,7 +15,7 @@ import Effect.Class (liftEffect)
 import Effect.Ref as Ref
 import Gesso.Canvas as GC
 import Gesso.Environment (Environment)
-import Gesso.GessoM (GessoM, runGessoM)
+import Gesso.GessoM (GessoM, runGessoM, class ManageState)
 import Halogen as H
 import Halogen.Aff (awaitBody, awaitLoad, selectElement) as Halogen.Aff
 import Halogen.Aff as HAff
@@ -25,34 +26,45 @@ import Web.HTML.HTMLElement (HTMLElement)
 runGessoAff :: forall x. Aff x -> Effect Unit
 runGessoAff = HAff.runHalogenAff
 
+-- I don't love this
 type Input appState r
   = { appState :: appState | r }
 
--- Top-level app - no other UI
+-- Top-level app - no other UI. Returns env
 run ::
   forall appState r q o.
   H.Component HTML q (Input appState r) o (GessoM appState ()) ->
   (Input appState r) ->
   HTMLElement ->
   Aff (Environment appState ())
---
--- how do I make it so I don't have to pass initial twice?
--- (in config and alone)
---
 run component config element = do
-  env <- liftEffect $ mkPlainEnv config.appState
-  io <- runUI (H.hoist (runGessoM env) component) config element
+  env <- liftEffect mkPlainEnv
+  io <- runUI (hoist env component) config element
   pure env
+  where
+  mkPlainEnv :: Effect (Environment appState ())
+  mkPlainEnv = do
+    appState <- Ref.new config.appState
+    stateBus <- Bus.make
+    pure { appState, stateBus }
 
--- Mid-level?
---  ::
---  =
---
--- Halogen component, can be embedded in others
---  ::
---  =
-mkPlainEnv :: forall appState. appState -> Effect (Environment appState ())
-mkPlainEnv initial = do
-  appState <- Ref.new initial
-  stateBus <- Bus.make
-  pure { appState, stateBus }
+  hoist ::
+    forall appState q i o e.
+    Environment appState e ->
+    H.Component HTML q i o (GessoM appState e) ->
+    H.Component HTML q i o Aff
+  hoist = H.hoist <<< runGessoM
+
+--Supply your own ManageState monad
+runWith ::
+  forall appState q i o m.
+  MonadAff m =>
+  ManageState m appState =>
+  m ~> Aff ->
+  H.Component HTML q i o m ->
+  i ->
+  HTMLElement ->
+  Aff Unit
+runWith runM component config element = do
+  _ <- runUI (H.hoist runM component) config element
+  pure unit
