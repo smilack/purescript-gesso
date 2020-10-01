d250ea6ded6766574c64fc42fbefb37684887d32
Tom Smilack <tom@smilack.net>
2020-09-30 19:41:56 -0500

    Add drawing/undoing pixels

--- examples/paint-global/src/Root.purs
+++ examples/paint-global/src/Root.purs
@@ -4,7 +4,8 @@ import Prelude
 import ColorButton as CB
 import Data.Array (range)
 import Data.Int (floor, toNumber)
-import Data.Maybe (Maybe(..))
+import Data.List (List(..), (:), tail, reverse)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Foldable (sequence_, traverse_)
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
@@ -20,13 +21,18 @@ import Gesso.Time as GTime
 import Graphics.Canvas as Canvas
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 
 type AppState
   = { mouseCell :: Maybe { x :: Int, y :: Int }
     , clicked :: Maybe { x :: Number, y :: Number }
     , showGrid :: Boolean
     , color :: String
+    , pixels :: List Pixel
     }
+
+newtype Pixel
+  = Pixel { x :: Int, y :: Int, color :: String }
 
 type Slots
   = ( colorButton :: CB.Slot Int
@@ -35,6 +41,7 @@ type Slots
 
 data Action
   = ButtonClicked CB.Output
+  | Undo
 
 initialState :: forall i. i -> AppState
 initialState _ =
@@ -42,6 +49,7 @@ initialState _ =
   , clicked: Nothing
   , showGrid: true
   , color: "black"
+  , pixels: Nil
   }
 
 component ::
@@ -64,6 +72,7 @@ render state =
     , HH.slot CB._colorButton 1 CB.component "#888" (Just <<< ButtonClicked)
     , HH.slot CB._colorButton 2 CB.component "#ccc" (Just <<< ButtonClicked)
     , HH.slot CB._colorButton 3 CB.component "white" (Just <<< ButtonClicked)
+    , HH.button [ HE.onClick (Just <<< const Undo) ] [ HH.text "undo" ]
     , HH.div []
         [ HH.slot GC._gessoCanvas unit GC.component (canvasInput state) absurd ]
     ]
@@ -73,9 +82,15 @@ handleAction ::
   MonadAff m =>
   ManageState m AppState =>
   Action -> H.HalogenM AppState Action s o m Unit
-handleAction (ButtonClicked (CB.Clicked color)) = do
-  appState <- GM.getState
-  GM.putState $ appState { color = color }
+handleAction = case _ of
+  ButtonClicked (CB.Clicked color) -> do
+    appState <- GM.getState
+    GM.putState $ appState { color = color }
+  Undo -> do
+    appState <- GM.getState
+    let
+      pixels' = fromMaybe Nil $ tail appState.pixels
+    GM.putState $ appState { pixels = pixels' }
 
 canvasInput :: AppState -> GC.Input AppState
 canvasInput appState =
@@ -95,42 +110,48 @@ canvasInput appState =
   }
 
 highlightCell ::
-  forall r i.
-  GInt.Interaction GEv.MouseEvent { mouseCell :: Maybe { x :: Int, y :: Int } | r } i
+  forall i.
+  GInt.Interaction GEv.MouseEvent AppState i
 highlightCell = GInt.mkInteraction GEv.onMouseMove getMousePos
   where
-  getMousePos event { toVb } state =
-    let
-      point = GDim.fromMouseEvent event
+  getMousePos event scaler = (_ { mouseCell = Just $ toXY event scaler })
 
-      { x, y } = { x: GDim.getX point, y: GDim.getY point }
+toXY :: GEv.MouseEvent -> GDim.Scaler -> { x :: Int, y :: Int }
+toXY event { toVb } =
+  let
+    point = GDim.fromMouseEvent event
 
-      x' = floor $ toVb.x' x
+    { x, y } = { x: GDim.getX point, y: GDim.getY point }
 
-      y' = floor $ toVb.y' y
-    in
-      state { mouseCell = Just { x: x', y: y' } }
+    x' = floor $ toVb.x' x
+
+    y' = floor $ toVb.y' y
+  in
+    { x: x', y: y' }
 
 clearHighlight ::
-  forall r i.
-  GInt.Interaction GEv.MouseEvent { mouseCell :: Maybe { x :: Int, y :: Int } | r } i
+  forall i.
+  GInt.Interaction GEv.MouseEvent AppState i
 clearHighlight = GInt.mkInteraction GEv.onMouseOut (\_ _ s -> s { mouseCell = Nothing })
 
 mouseDown ::
-  forall r i.
-  GInt.Interaction GEv.MouseEvent { clicked :: Maybe { x :: Number, y :: Number } | r } i
+  forall i.
+  GInt.Interaction GEv.MouseEvent AppState i
 mouseDown = GInt.mkInteraction GEv.onMouseDown getMousePos
   where
-  getMousePos event _ state =
+  getMousePos event scaler state =
     let
-      point = GDim.fromMouseEvent event
+      { x, y } = toXY event scaler
+
+      p = Pixel { x, y, color: state.color }
     in
-      state { clicked = Just { x: GDim.getX point, y: GDim.getY point } }
+      state { pixels = p : state.pixels }
 
 renderApp :: AppState -> GTime.Delta -> GDim.Scaler -> Canvas.Context2D -> Effect Unit
-renderApp { clicked, mouseCell, showGrid, color } _ { x_, y_, w_, h_, screen, toVb } context = do
+renderApp { clicked, mouseCell, showGrid, color, pixels } _ { x_, y_, w_, h_, screen, toVb } context = do
   clearBackground
   when showGrid drawGrid
+  drawImage
   traverse_ drawCursor mouseCell
   where
   clearBackground :: Effect Unit
@@ -160,3 +181,11 @@ renderApp { clicked, mouseCell, showGrid, color } _ { x_, y_, w_, h_, screen, to
   drawCursor { x, y } = do
     Canvas.setFillStyle context color
     Canvas.fillRect context { x: x_ (toNumber x), y: y_ (toNumber y), width: w_ 1.0, height: h_ 1.0 }
+
+  drawImage :: Effect Unit
+  drawImage = sequence_ $ map drawPixel $ reverse pixels
+
+  drawPixel :: Pixel -> Effect Unit
+  drawPixel (Pixel { x, y, color: c }) = do
+    Canvas.setFillStyle context c
+    Canvas.fillRect context { x: x_ $ toNumber x, y: y_ $ toNumber y, width: w_ 1.0, height: h_ 1.0 }
