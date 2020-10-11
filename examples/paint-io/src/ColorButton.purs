ca171e37e90ea816c1e259467e36344f234850c4
Tom Smilack <tom@smilack.net>
2020-10-11 17:20:04 -0500

    Improve color buttons
    Make the color buttons larger, go down the left side, show the color instead of the name, and display outline if selected

--- examples/paint-io/src/ColorButton.purs
+++ examples/paint-io/src/ColorButton.purs
@@ -6,6 +6,7 @@ import Data.Symbol (SProxy(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events (onClick)
+import Halogen.HTML.Properties as HP
 
 type Slot slot
   = forall q. H.Slot q Output slot
@@ -13,16 +14,17 @@ type Slot slot
 _colorButton = SProxy :: SProxy "colorButton"
 
 type State
-  = String
+  = { color :: String, selected :: String }
 
 type Input
-  = String
+  = State
 
 data Output
-  = Clicked State
+  = Clicked String
 
 data Action
-  = ClickedAction State
+  = ClickedAction String
+  | Update Input
 
 component :: forall q m. H.Component HH.HTML q Input Output m
 component =
@@ -32,14 +34,33 @@ component =
     , eval:
         H.mkEval
           $ H.defaultEval
-              { handleAction = handleAction }
+              { handleAction = handleAction, receive = Just <<< Update }
     }
 
 render :: forall s m. State -> H.ComponentHTML Action s m
-render state =
+render { color, selected } =
   HH.button
-    [ onClick \_ -> Just $ ClickedAction state ]
-    [ HH.text state ]
+    [ onClick \_ -> Just $ ClickedAction color
+    , style
+        $ ("background-color: " <> color <> ";")
+        <> "width: 72px;"
+        <> "height: 72px;"
+        <> "margin: 6px 12px;"
+        <> "cursor: pointer;"
+        <> selectedStyle
+    ]
+    []
+  where
+  selectedStyle =
+    if selected == color then
+      "outline: 4px #2196f3 solid;"
+    else
+      ""
+
+style :: forall r i. String -> HP.IProp r i
+style = HP.attr (HH.AttrName "style")
 
 handleAction :: forall s m. Action -> H.HalogenM State Action s Output m Unit
-handleAction (ClickedAction state) = H.raise $ Clicked state
+handleAction = case _ of
+  ClickedAction state -> H.raise $ Clicked state
+  Update state -> H.put state
