4efdccadf18242959398cadcb834edb1b361abdd
Tom Smilack <tom@smilack.net>
2020-10-18 17:43:38 -0500

    Change Input type signature for Gesso Input changes

--- examples/unit-grid/src/Main.purs
+++ examples/unit-grid/src/Main.purs
@@ -27,7 +27,7 @@ type LocalState
     , clicked :: Maybe { x :: Number, y :: Number }
     }
 
-input :: GC.Input LocalState Unit Void
+input :: GC.Input LocalState Unit Void Void
 input =
   { name: "test-app"
   , localState: { mousePos: Nothing, clicked: Nothing }
