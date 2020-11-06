a21db25c27d3e68ad349b4dc1e7e1779cdeb4bde
Tom Smilack <tom@smilack.net>
2020-11-06 01:00:04 -0600

    Document Environment module

--- src/Gesso/Environment.purs
+++ src/Gesso/Environment.purs
@@ -1,3 +1,5 @@
+-- | The global Environment that `GessoM` and the `ManageState` typeclass run
+-- | under.
 module Gesso.Environment (Environment(..), busEventSource) where
 
 import Prelude
@@ -8,12 +10,18 @@ import Effect.Aff.Class (class MonadAff)
 import Effect.Ref (Ref)
 import Halogen.Query.EventSource (EventSource, affEventSource, Finalizer(..), emit)
 
+-- | The environment must contain a `Ref` to the global state of the application
+-- | and a Bus for notifying subscribers of changes, but may contain other
+-- | fields as well.
 type Environment globalState more
   = { globalState :: Ref globalState
     , stateBus :: BusRW globalState
     | more
     }
 
+-- | Convert a bus to a Halogen event source. It's unlikely that you want to
+-- | call this function - `getEventSource` from `ManageState` in `GessoM` gives
+-- | you the event source directly.
 busEventSource ::
   forall globalState m r.
   MonadAff m =>
