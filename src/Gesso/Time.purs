982edf69836aa929713e8e3bd28f5ab83d061256
Tom Smilack <tom@smilack.net>
2020-11-06 12:48:51 -0600

    Document Time

--- src/Gesso/Time.purs
+++ src/Gesso/Time.purs
@@ -1,3 +1,4 @@
+-- | Timestamps, time deltas, and `requestAnimationFrame`
 module Gesso.Time
   ( requestAnimationFrame
   , module Web.HTML.Window
@@ -20,29 +21,42 @@ import Web.HTML.Window (RequestAnimationFrameId, cancelAnimationFrame)
 
 foreign import _requestAnimationFrame :: EffectFn1 Number Unit -> Window -> Effect Int
 
+-- | An interface to JavaScript's `window.requestAnimationFrame` which provides
+-- | the timestamp argument to the callback function. `requestAnimationFrame` in
+-- | `Web.HTML.Window` only accepts and `Effect Unit` instead of a function.
 requestAnimationFrame :: (TimestampCurrent -> Effect Unit) -> Window -> Effect RequestAnimationFrameId
 requestAnimationFrame fn = map wrap <<< _requestAnimationFrame (mkEffectFn1 $ fn <<< wrap)
 
+-- | A number representing a specific time in milliseconds. See
+-- | [`DOMHighResTimeStamp`](https://developer.mozilla.org/en-US/docs/Web/API/DOMHighResTimeStamp)
 newtype Timestamp a
   = Timestamp Number
 
 derive instance newtypeTimestamp :: Newtype (Timestamp a) _
 
+-- | A phantom type for tagging Timestamps
 data Now
 
+-- | A phantom type for tagging Timestamps
 data Prev
 
+-- | Alias for a timestamp representing the time the current frame started
 type TimestampCurrent
   = Timestamp Now
 
+-- | Alias for a timestamp representing the time the previous frame started
 type TimestampPrevious
   = Timestamp Prev
 
+-- | Mark a current time as the new previous time
 toPrev :: TimestampCurrent -> TimestampPrevious
 toPrev = wrap <<< unwrap
 
+-- | A record containing a current timestamp, the previous timestamp, and the
+-- | difference between them. All values are in milliseconds.
 type Delta
   = { now :: TimestampCurrent, prev :: TimestampPrevious, delta :: Number }
 
+-- | Create a Delta record from a current and a previous timestamp.
 delta :: TimestampCurrent -> TimestampPrevious -> Delta
 delta now@(Timestamp n) prev@(Timestamp p) = { prev, now, delta: n - p }
