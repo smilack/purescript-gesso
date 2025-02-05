8295d69c731137d9ecaa6bc07d20a1dd2c3cb335
Tom Smilack <tom@smilack.net>
2025-02-04 23:30:55 -0600

    Apparently a lot of the typeclass stuff I was doing to enable generically applying record functions is unnecessary if you make it even more generic

--- src/Gesso/Dimensions3.purs
+++ src/Gesso/Dimensions3.purs
@@ -4,7 +4,7 @@ module Gesso.Dimensions3 where
 import Prelude
 
 import Record (get, set, modify, insert, delete) as R
-import Record.Builder (modify, insert, delete, build, buildFromScratch)
+import Record.Builder (modify, insert, delete, build, buildFromScratch, Builder)
 import Type.Prelude (class IsSymbol)
 import Type.Proxy (Proxy(..))
 import Type.Row (class Lacks, type (+), RowApply, class Cons)
@@ -51,57 +51,95 @@ import Type.RowList (RowList, Nil, Cons, class RowToList, class ListToRow)
      5. returns scaled `x` and `y`
 -}
 
-type Converter :: Symbol -> Type
-type Converter sym =
+type Converter :: Symbol -> Type -> Type
+type Converter sym a =
   forall tail row
-   . Cleave tail row (Proxy sym)
+   . Cleave a tail row (Proxy sym)
   => Record row
   -> Record row
 
-converter :: forall @sym. Converter sym
-converter rec =
+converter :: forall @sym @a. (a -> a) -> Converter sym a
+converter f rec =
   let
-    x = get (Proxy @sym) rec
-    rec' = uncon (Proxy @sym) rec
-    rec'' = con (Proxy @sym) x rec'
+    x = get @a (Proxy @sym) rec
+    rec' = uncon @a (Proxy @sym) rec
+    rec'' = con @a (Proxy @sym) (f x) rec'
   in
     rec''
 
+builderConv
+  :: forall @sym @a tail row
+   . Cleave a tail row (Proxy sym)
+  => (a -> a)
+  -> Builder { | row } { | row }
+builderConv = modify2 (Proxy @sym)
+
+conv2
+  :: forall sym a tail row
+   . IsSymbol sym
+  => Cons sym a tail row
+  => (a -> a)
+  -> { | row }
+  -> { | row }
+conv2 = R.modify (Proxy @sym)
+
+builderConv2
+  :: forall sym a tail row
+   . IsSymbol sym
+  => Cons sym a tail row
+  => (a -> a)
+  -> Builder { | row } { | row }
+builderConv2 = modify (Proxy @sym)
+
 converters
-  :: { x :: Converter "x"
-     , y :: Converter "y"
-     , width :: Converter "width"
-     , height :: Converter "height"
+  :: { x :: Converter "x" Number
+     , y :: Converter "y" Number
+     , width :: Converter "width" Number
+     , height :: Converter "height" Number
      }
 converters =
-  { x: converter @"x"
-  , y: converter @"y"
-  , width: converter @"width"
-  , height: converter @"height"
+  { x: converter @"x" (_ + 1.0)
+  , y: converter @"y" (_ + 1.0)
+  , width: converter @"width" (_ + 1.0)
+  , height: converter @"height" (_ + 1.0)
   }
 
--- rx = converters.x { x: 1.0 }
--- ry = converters.x { y: 1.0, x: 1.0 }
+rx = converters.x { x: 1.0, y: 1.0, w: 1.0, h: 1.0 }
+ry = converters.y { y: 1.0, x: 1.0 }
+
+-- rxy = converters.y $ converters.x { y: 1.0, x: 1.0, width: 1.0 }
 
 -- | Like Prim.Row.Cons but with operations to get/insert/delete the field
-class Cleave :: Row Type -> Row Type -> Type -> Constraint
-class Cleave tail row symp | symp -> tail row where
-  get :: symp -> { | row } -> Number
-  con :: symp -> Number -> { | tail } -> { | row }
+class Cleave :: Type -> Row Type -> Row Type -> Type -> Constraint
+class Cleave a tail row symp | a symp -> tail row where
+  get :: symp -> { | row } -> a
+  con :: symp -> a -> { | tail } -> { | row }
   uncon :: symp -> { | row } -> { | tail }
+  con2 :: symp -> a -> Builder { | tail } { | row }
+  uncon2 :: symp -> Builder { | row } { | tail }
+  modify2 :: symp -> (a -> a) -> Builder { | row } { | row }
 
 instance
   ( IsSymbol sym
-  , Cons sym Number tail row
+  , Cons sym a tail row
   , Lacks sym tail
   ) =>
-  Cleave tail row (Proxy sym)
+  Cleave a tail row (Proxy sym)
   where
-  get :: Proxy sym -> { | row } -> Number
+  get :: Proxy sym -> { | row } -> a
   get = R.get
 
-  con :: Proxy sym -> Number -> { | tail } -> { | row }
+  con :: Proxy sym -> a -> { | tail } -> { | row }
   con = R.insert
 
   uncon :: Proxy sym -> { | row } -> { | tail }
   uncon = R.delete
+
+  con2 :: Proxy sym -> a -> Builder { | tail } { | row }
+  con2 = insert
+
+  uncon2 :: Proxy sym -> Builder { | row } { | tail }
+  uncon2 = delete
+
+  modify2 :: Proxy sym -> (a -> a) -> Builder { | row } { | row }
+  modify2 = modify
