38351903c76b19d2a5e84b1a7a2e67daa53f715e
Tom Smilack <tom@smilack.net>
2025-02-06 17:55:48 -0600

    Working on pulling the working things from Dimensions3 to Scaler. Having some issues getting the types to line up when putting everything together - there may be a few too many foralls in the aliases, or maybe some of the Cons classes are mutually exclusive

--- src/Gesso/Scale.purs
+++ src/Gesso/Scale.purs
@@ -2,43 +2,186 @@ module Gesso.Scale where
 
 import Prelude
 
-import Data.Symbol (class IsSymbol)
-import Record (delete, get, insert, modify)
+import Record.Builder (modify, build, Builder, flip, merge)
+import Record.Extra (class Keys, pick)
+import Type.Prelude (class IsSymbol)
 import Type.Proxy (Proxy(..))
-import Type.Row (class Cons, RowApply, type (+), class Lacks)
-import Type.RowList (RowList, class RowToList, class ListToRow, Cons, Nil)
+import Type.Row (class Lacks, type (+), RowApply, class Cons, class Union, class Nub)
+import Type.RowList (RowList, Nil, Cons, class RowToList, class ListToRow, class RowListAppend, class RowListNub)
 
-type And :: (RowList Type -> RowList Type) -> RowList Type -> RowList Type
-type And a b = a b
+-- ┌───────────────────────────┐
+-- │ Dimension & scaling types │
+-- └───────────────────────────┘
 
-infixr 0 type And as <>
+type X r = (x :: Number | r)
+type Y r = (y :: Number | r)
+type Width r = (width :: Number | r)
+type Height r = (height :: Number | r)
 
-type AndLast :: (RowList Type -> RowList Type) -> (RowList Type -> RowList Type) -> RowList Type
-type AndLast a b = a (b Nil)
+type Scalable r = X + Y + Width + Height + r
 
-infixr 0 type AndLast as <|
+-- | Alias for record builder that applies a function to one or more fields in a
+-- | record without changing their types.
+type ScalerFor :: (Row Type -> Row Type) -> Row Type -> Type
+type ScalerFor f r = Builder { | f r } { | f r }
 
-type Qty :: Symbol -> RowList Type -> RowList Type
-type Qty k = Cons k Number
+type ScalingFunctions =
+  { x :: Number -> Number
+  , y :: Number -> Number
+  , width :: Number -> Number
+  , height :: Number -> Number
+  }
 
-type X = Qty "x"
-type Y = Qty "y"
-type Width = Qty "width"
-type Height = Qty "height"
+-- | A record containing scalers for all scalable fields.
+type Scalers r =
+  { x :: ScalerFor X r
+  , y :: ScalerFor Y r
+  , width :: ScalerFor Width r
+  , height :: ScalerFor Height r
+  , all :: ScalerFor Scalable r
+  }
 
-type Point = X <| Y
-type Size = Width <| Height
-type Rect = X <> Y <> Width <| Height
+-- ┌────────────────────┐
+-- │ Scaling operations │
+-- └────────────────────┘
 
-data Field :: Symbol -> Type -> Row Type -> Row Type -> Type
-data Field label a tail row
+defaults :: Record (Scalable ())
+defaults = { x: 0.0, y: 0.0, width: 0.0, height: 0.0 }
 
-class ScalableField
-  :: Type
-  -> Type
-  -> Constraint
-class ScalableField f rec where
-  scale :: rec -> rec
+fill
+  :: forall partial union complete
+   . Union partial (Scalable ()) union
+  => Nub union complete
+  => Builder (Record partial) (Record complete)
+fill = flip merge defaults
+
+-- extract
+--   :: forall keys @required from all diff filler
+--    . RowToList required keys
+--   => RowToList all from
+--   => Del keys from diff
+--   => Union required filler all
+--   => Keys keys
+--   => { | required }
+--   -> { | all }
+--   -> { | required }
+scale
+  :: forall r union all
+   . Union r (Scalable ()) union
+  => Nub union all
+  => Scalers r
+  -> Record r
+  -> Record r
+scale { all } r =
+  extract @r @all r
+    $ build (all <<< fill) r
+
+mkScaler
+  :: forall @sym a r row
+   . IsSymbol sym
+  => Cons sym a r row
+  => (a -> a)
+  -> Builder (Record row) (Record row)
+mkScaler = modify (Proxy @sym)
+
+mkScalers :: forall r. ScalingFunctions -> Scalers r
+mkScalers fs = { x, y, width, height, all }
+  where
+  x :: ScalerFor X r
+  x = mkScaler @"x" fs.x
+
+  y :: ScalerFor Y r
+  y = mkScaler @"y" fs.y
+
+  width :: ScalerFor Width r
+  width = mkScaler @"width" fs.width
+
+  height :: ScalerFor Height r
+  height = mkScaler @"height" fs.height
+
+  all :: ScalerFor Scalable r
+  all = x <<< y <<< width <<< height
+
+-- ┌──────────────────────────┐
+-- │ Row Difference typeclass │
+-- └──────────────────────────┘
+
+-- | Find keys in `from` that don't exist in `keys`. `from` is assumed to be a
+-- | superset of `keys`, which simplifies a couple things:
+-- |
+-- |  - There's no need for an instance where `from` is `Nil` but `keys` is not.
+-- |
+-- |  - When the heads of `keys` and `from` are different, we only need to
+-- |    advance `from`, because the key will be in there later.
+class Del :: RowList Type -> RowList Type -> RowList Type -> Constraint
+class Del keys from diff | keys from -> diff
+
+instance delEmpty :: Del Nil Nil Nil
+
+else instance delNoMoreKeys :: Del Nil from from
+
+else instance delDeleteKey ::
+  ( Del keyTail fromTail diff
+  ) =>
+  Del (Cons key a keyTail) (Cons key a fromTail) diff
+
+else instance delKeepKey ::
+  ( Del (Cons key a keyTail) fromTail diffTail
+  ) =>
+  Del (Cons key a keyTail) (Cons key' b fromTail) (Cons key' b diffTail)
+
+-- | Find values of all keys in `all` that do not appear in `required`.
+delete
+  :: forall keys required from all diff filler
+   . RowToList required keys
+  => RowToList all from
+  => Del keys from diff
+  => RowToList filler diff
+  => Union filler required all
+  => Keys diff
+  => { | required }
+  -> { | all }
+  -> { | filler }
+delete _ = pick
+
+-- | Find values of all keys in `all` that also appear in `required`.
+extract
+  :: forall keys @required from @all diff filler
+   . RowToList required keys
+  => RowToList all from
+  => Del keys from diff
+  => Union required filler all
+  => Keys keys
+  => { | required }
+  -> { | all }
+  -> { | required }
+extract _ = pick
+
+-- ┌───────────────┐
+-- │ Utility types │
+-- └───────────────┘
+
+-- | Alias just so I don't have to break long lines
+type OpenRow = Row Type -> Row Type
+
+-- | Combine two open rows and close them
+type RowApplyClose :: OpenRow -> OpenRow -> Row Type
+type RowApplyClose a b = a + b + ()
+
+infixr 0 type RowApplyClose as &
+
+-- | Combine two open rows and leave them open
+type RowApplyOpen :: OpenRow -> OpenRow -> OpenRow
+type RowApplyOpen a b r = a + b + r
+
+infixr 1 type RowApplyOpen as ++
+
+{-
+x:      -@   @>
+width:  -@-  @>>
+y:      |@   @^
+height: |@|  @^^
+-}
 
 -- instance
 --   -- ( Cons "x" Number r r1
@@ -58,97 +201,3 @@ class ScalableField f rec where
 --       rec
 
 -- Field rec
-
-newtype Scaler a = Scaler a
-
--- ┌───────────────────────────────────────────────────────────┐
--- │ TravRec: Newtype with instance of TraversableRecord class │
--- │ Convenience method "sequence":                            │
--- │   Use "sequenceRecord" without creating a TravRec         │
--- └───────────────────────────────────────────────────────────┘
-
-newtype TravRec :: (Type -> Type) -> Row Type -> Type
-newtype TravRec applic rowAp = TravRec (Record rowAp)
-
-sequence
-  :: forall f
-       (r :: Row Type) (r' :: Row Type)
-       (rec :: Type) (@rec' :: Type)
-   . Applicative f
-  => RecordToRow rec r
-  => RecordToRow rec' r'
-  => TraversableRecord TravRec f r r'
-  => rec
-  -> f rec'
-sequence =
-  map (toRec :: Record r' -> rec')
-    <<<
-      ( sequenceRecord @TravRec @f @r @r'
-          :: TravRec f r -> f (Record r')
-      )
-    <<< (TravRec :: Record r -> TravRec f r)
-    <<< (toRow :: rec -> Record r)
-
--- ┌───────────────────────────────────────────────────────────┐
--- │ RecordToRow: Equates a Row Type, RowList Type, and Record │
--- └───────────────────────────────────────────────────────────┘
-
-class RecordToRow :: Type -> Row Type -> Constraint
-class RecordToRow rec row | rec -> row, row -> rec where
-  toRow :: rec -> { | row }
-  toRec :: { | row } -> rec
-
-instance RecordToRow { | row } row where
-  toRow = identity
-  toRec = identity
-
--- ┌───────────────────────────────────────────────────┐
--- │ TraversableRecord: class and instance for TravRec │
--- └───────────────────────────────────────────────────┘
-
-class TraversableRecord
-  :: ((Type -> Type) -> Row Type -> Type) -- applic, rowAp, newtype
-  -> (Type -> Type) -- applic
-  -> Row Type -- rowAp
-  -> Row Type -- rowNoAp
-  -> Constraint
-class TraversableRecord newty applic rowAp rowNoAp where
-  sequenceRecord
-    :: newty applic rowAp -> applic (Record rowNoAp)
-
-instance
-  ( RowToList rowAp Nil
-  , Applicative applic
-  ) =>
-  TraversableRecord TravRec applic rowAp rowAp where
-  sequenceRecord
-    :: TravRec applic rowAp -> applic (Record rowAp)
-  sequenceRecord (TravRec rec) = pure rec
-
-else instance
-  ( RowToList rowAp (Cons key (applic val) listAp')
-  , RowToList rowAp' listAp'
-  , RowToList rowNoAp (Cons key val listNoAp')
-  , RowToList rowNoAp' listNoAp'
-  , Applicative applic
-  , IsSymbol key
-  , Cons key (applic val) rowAp' rowAp
-  , Cons key val rowNoAp' rowNoAp
-  , Lacks key rowAp'
-  , Lacks key rowNoAp'
-  , TraversableRecord TravRec applic rowAp' rowNoAp'
-  ) =>
-  TraversableRecord TravRec applic rowAp rowNoAp where
-  sequenceRecord
-    :: TravRec applic rowAp -> applic (Record rowNoAp)
-  sequenceRecord (TravRec rec) =
-    let
-      key = Proxy @key :: Proxy key
-      val = get key rec :: applic val
-      rec' = delete key rec :: Record rowAp'
-      trec' = TravRec rec' :: TravRec applic rowAp'
-      seqRec' = sequenceRecord trec' :: applic (Record rowNoAp')
-      ins = insert key <$> val :: applic (Record rowNoAp' -> Record rowNoAp)
-      result = ins <*> seqRec' :: applic (Record rowNoAp)
-    in
-      result
