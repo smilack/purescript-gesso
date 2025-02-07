9dcc0f330b67b6680aec4c6a29677b0d0c3c237c
Tom Smilack <tom@smilack.net>
2025-02-07 17:24:38 -0600

    Everything is hooked up and ready for testing!

--- src/Gesso/Scale.purs
+++ src/Gesso/Scale.purs
@@ -1,4 +1,25 @@
-module Gesso.Scale where
+module Gesso.Scale
+  ( (@>)
+  , (@>>)
+  , (@@)
+  , (@^)
+  , (@^^)
+  , CoordinateSystem(..)
+  , Dimensioned
+  , Dimensioned'
+  , Drawing
+  , Page
+  , Rect
+  , Rect'
+  , Scaler
+  , ScalingFunctions
+  , mkScalers
+  , scale
+  , scaleHeight
+  , scaleWidth
+  , scaleX
+  , scaleY
+  ) where
 
 import Prelude hiding (flip)
 
@@ -7,7 +28,7 @@ import Record.Extra (class Keys, pick)
 import Type.Prelude (class IsSymbol)
 import Type.Proxy (Proxy(..))
 import Type.Row (class Cons, class Union, class Nub)
-import Type.RowList (RowList, Nil, Cons, class RowToList)
+import Type.RowList (class RowToList)
 
 -- ┌───────────────────────────┐
 -- │ Dimension & scaling types │
@@ -31,11 +52,20 @@ type Rect' = Dimensioned' Number ()
 type Rect :: Type
 type Rect = Record Rect'
 
-type ScalingFunctions :: Type
-type ScalingFunctions = { | Dimensioned' (Number -> Number) () }
+type ScalingFunctions :: Row Type
+type ScalingFunctions = Dimensioned' (Number -> Number) ()
 
-type Scaler :: Row Type -> Type
-type Scaler r = Builder (Record r) (Record r)
+data CoordinateSystem
+
+foreign import data Page :: CoordinateSystem
+
+foreign import data Drawing :: CoordinateSystem
+
+type Scaler :: CoordinateSystem -> Row Type -> Type
+type Scaler c r =
+  { scaler :: Builder (Record r) (Record r)
+  | ScalingFunctions
+  }
 
 -- ┌────────────────────┐
 -- │ Scaling operations │
@@ -52,18 +82,34 @@ fill
 fill = flip merge defaults
 
 scale
-  :: forall partial union complete keys from diff filler
-   . Union partial Rect' union
-  => Nub union complete
+  :: forall partial u complete keys filler c
+   . Union partial Rect' u
+  => Nub u complete
   => RowToList partial keys
-  => RowToList complete from
-  => Del keys from diff
   => Union partial filler complete
   => Keys keys
-  => Scaler complete
+  => Record partial
+  -> Scaler c complete
   -> Record partial
-  -> Record partial
-scale scaler r = extract r $ build (scaler <<< fill) r
+scale r { scaler } = pick $ build (scaler <<< fill) r
+
+scaleX :: forall c r. Number -> Scaler c r -> Number
+scaleX n { x } = x n
+
+scaleY :: forall c r. Number -> Scaler c r -> Number
+scaleY n { y } = y n
+
+scaleWidth :: forall c r. Number -> Scaler c r -> Number
+scaleWidth n { width } = width n
+
+scaleHeight :: forall c r. Number -> Scaler c r -> Number
+scaleHeight n { height } = height n
+
+infix 2 scale as @@
+infix 2 scaleX as @>
+infix 2 scaleY as @^
+infix 2 scaleWidth as @>>
+infix 2 scaleHeight as @^^
 
 mkScaler
   :: forall @l a t r
@@ -73,71 +119,13 @@ mkScaler
   -> Builder (Record r) (Record r)
 mkScaler = modify (Proxy @l)
 
-mkScalers :: forall r. ScalingFunctions -> Scaler (Dimensioned' Number r)
-mkScalers fs =
-  mkScaler @"x" fs.x
-    <<< mkScaler @"y" fs.y
-    <<< mkScaler @"width" fs.width
-    <<< mkScaler @"height" fs.height
-
--- ┌──────────────────────────┐
--- │ Row Difference typeclass │
--- └──────────────────────────┘
-
--- | Find keys in `from` that don't exist in `keys`. `from` is assumed to be a
--- | superset of `keys`, which simplifies a couple things:
--- |
--- |  - There's no need for an instance where `from` is `Nil` but `keys` is not.
--- |
--- |  - When the heads of `keys` and `from` are different, we only need to
--- |    advance `from`, because the key will be in there later.
-class Del :: RowList Type -> RowList Type -> RowList Type -> Constraint
-class Del keys from diff | keys from -> diff
-
-instance delEmpty :: Del Nil Nil Nil
-
-else instance delNoMoreKeys :: Del Nil from from
-
-else instance delDeleteKey ::
-  ( Del keyTail fromTail diff
-  ) =>
-  Del (Cons key a keyTail) (Cons key a fromTail) diff
-
-else instance delKeepKey ::
-  ( Del (Cons key a keyTail) fromTail diffTail
-  ) =>
-  Del (Cons key a keyTail) (Cons key' b fromTail) (Cons key' b diffTail)
-
--- | Find values of all keys in `all` that do not appear in `required`.
-delete
-  :: forall keys required from all diff filler
-   . RowToList required keys
-  => RowToList all from
-  => Del keys from diff
-  => RowToList filler diff
-  => Union filler required all
-  => Keys diff
-  => { | required }
-  -> { | all }
-  -> { | filler }
-delete _ = pick
-
--- | Find values of all keys in `all` that also appear in `required`.
-extract
-  :: forall keys required from all diff filler
-   . RowToList required keys
-  => RowToList all from
-  => Del keys from diff
-  => Union required filler all
-  => Keys keys
-  => { | required }
-  -> { | all }
-  -> { | required }
-extract _ = pick
-
-{-
-x:      -@   @>
-width:  -@-  @>>
-y:      |@   @^
-height: |@|  @^^
--}
+mkScalers
+  :: forall @c r
+   . Record ScalingFunctions
+  -> Scaler c (Dimensioned' Number r)
+mkScalers { x, y, width, height } = { scaler, x, y, width, height }
+  where
+  scaler = mkScaler @"x" x
+    <<< mkScaler @"y" y
+    <<< mkScaler @"width" width
+    <<< mkScaler @"height" height
