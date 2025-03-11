ebae11b26cd6101bec570761d7d44eae9a82b318
Tom Smilack <tom@smilack.net>
2025-03-10 22:53:19 -0500

    Make clock example slightly nicer

--- examples/clock/src/Main.purs
+++ examples/clock/src/Main.purs
@@ -15,7 +15,7 @@ import Gesso.Application as GApp
 import Gesso.Canvas (component, Input) as GC
 import Gesso.Dimensions as GDim
 import Gesso.Interactions as GInt
-import Gesso.Scale ((@>), (@>>), (@^), (@^^))
+import Gesso.Scale ((@>>), (@^^), to)
 import Gesso.Time as GTime
 import Gesso.Util.Lerp as GLerp
 import Graphics.Canvas as Canvas
@@ -43,7 +43,7 @@ input =
   }
 
 render :: Canvas.Context2D -> GTime.Delta -> GDim.Scaler -> GLerp.Lerp Unit -> Effect Unit
-render context _ { toRectangle, screen, viewBox, scaler: { canvas } } _ = do
+render context _ { toRectangle, screen, scaler: { canvas, drawing } } _ = do
   -- Clear background
   Canvas.setFillStyle context "white"
   Canvas.fillRect context (toRectangle screen)
@@ -60,10 +60,10 @@ render context _ { toRectangle, screen, viewBox, scaler: { canvas } } _ = do
     Canvas.arc context { x: clock.x, y: clock.y, start: 0.0, end: tau, radius: 15.0 @>> canvas, useCounterClockwise: false }
   where
   clock =
-    { x: (GDim.getWidth viewBox) / 2.0 @> canvas
-    , y: (GDim.getHeight viewBox) / 2.0 @^ canvas
-    , r: 500.0 @>> canvas
-    }
+    { x: drawing.width / 2.0
+    , y: drawing.height / 2.0
+    , r: 500.0
+    } `to` canvas
 
   eta = pi / 2.0
 
