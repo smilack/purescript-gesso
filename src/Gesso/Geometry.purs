17131496d5958b2978588bd208e6c6fa63be4fc4
Tom Smilack <tom@smilack.net>
2025-03-18 18:22:03 -0500

    Move 'mkScalers' into new Geometry.Internal module so it's not exported from Geometry. Move 'fromMouseEvent' function back into Geometry

--- src/Gesso/Geometry.purs
+++ src/Gesso/Geometry.purs
@@ -1,48 +1,20 @@
 module Gesso.Geometry
-  ( Scalers
-  , mkScalers
+  ( fromMouseEvent
   , module Exports
   ) where
 
 import Prelude
 
-import Gesso.Geometry.Dimensions (Rect, largestContainedArea)
+import Data.Int (toNumber)
 import Gesso.Geometry.Dimensions (Area, Point, Position, Rect, Rectangular, Size, null, origin, sizeless) as Exports
-import Gesso.Geometry.Scaler (Scaler, mkScaler)
+import Gesso.Geometry.Dimensions (Point)
+import Gesso.Geometry.Internal (Scalers) as Exports
 import Gesso.Geometry.Scaler ((>@), (@<~), (^@), (~>@), (-@), Scaler, from, lengthTo, to, xTo, yTo) as Exports
+import Web.UIEvent.MouseEvent (MouseEvent)
+import Web.UIEvent.MouseEvent (pageX, pageY) as MouseEvent
 
-type Scalers =
-  { scale :: Number
-  , canvas :: Scaler
-  , drawing :: Scaler
+fromMouseEvent :: MouseEvent -> Point
+fromMouseEvent event =
+  { x: toNumber $ MouseEvent.pageX event
+  , y: toNumber $ MouseEvent.pageY event
   }
-
-mkScalers :: Rect -> Rect -> Scalers
-mkScalers viewBox clientRect =
-  { scale: k
-  , drawing: mkScaler viewBox toDrawing
-  , canvas: mkScaler
-      clientRect { x = 0.0, y = 0.0 }
-      toCanvas
-  }
-  where
-  fullView = largestContainedArea viewBox clientRect
-
-  margin =
-    { width: (clientRect.width - fullView.width) / 2.0
-    , height: (clientRect.height - fullView.height) / 2.0
-    }
-
-  k = viewBox.width / fullView.width
-
-  toCanvas =
-    { x: (_ / k) >>> add margin.width >>> (_ - (viewBox.x / k))
-    , y: (_ / k) >>> add margin.height >>> (_ - (viewBox.y / k))
-    , length: (_ / k)
-    }
-
-  toDrawing =
-    { x: (_ - (clientRect.x + margin.width)) >>> mul k >>> add viewBox.x
-    , y: (_ - (clientRect.y + margin.height)) >>> mul k >>> add viewBox.y
-    , length: mul k
-    }
