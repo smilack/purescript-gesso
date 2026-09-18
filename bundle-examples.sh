fe3d910915e94bdaf5044b6f6ea06a93d2f1d921
Tom Smilack <tom@smilack.net>
2026-09-18 14:30:00 -0500

    Alphabetically sort examples in bundle-examples script

--- bundle-examples.sh
+++ bundle-examples.sh
@@ -1,9 +1,9 @@
 set -e
-spago bundle --source-maps -p gesso-example-bounce
 spago bundle --source-maps -p gesso-example-analog-clock
-spago bundle --source-maps -p gesso-example-keyboard
+spago bundle --source-maps -p gesso-example-bounce
 spago bundle --source-maps -p gesso-example-hello
-spago bundle --source-maps -p gesso-example-paint-app
-spago bundle --source-maps -p gesso-example-mouse-and-scaling
 spago bundle --source-maps -p gesso-example-interpolation
+spago bundle --source-maps -p gesso-example-keyboard
+spago bundle --source-maps -p gesso-example-mouse-and-scaling
+spago bundle --source-maps -p gesso-example-paint-app
 spago bundle --source-maps -p gesso-example-timing
